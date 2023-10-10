de33c400a761bc3eafb98268e7bc2b121757a178
Matt Waterman <mwaterman29@gmail.com>
2023-10-09 23:57:03 -0400

    yoinked css from work

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,33 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      animation:{
+        fadeIn1: 'fadeIn 1s ease-in-out forwards',
+        fadeIn2: 'fadeIn 1s ease-in-out 0.5s forwards',
+        fadeIn3: 'fadeIn 1s ease-in-out 1s forwards',
+        fadeIn4: 'fadeIn 1s ease-in-out 1.5s forwards',
+        fadeIn5: 'fadeIn 1s ease-in-out 2s forwards',
+        fadeIn6: 'fadeIn 1s ease-in-out 2.5s forwards',
+
+        fadeOut1: 'fadeOut 1s ease-in-out forwards',
+        fadeOut2: 'fadeOut 1s ease-in-out 0.5s forwards',
+        fadeOut3: 'fadeOut 1s ease-in-out 1s forwards',
+        fadeOut4: 'fadeOut 1s ease-in-out 1.5s forwards',
+        fadeOut5: 'fadeOut 1s ease-in-out 2s forwards',
+        fadeOut6: 'fadeOut 1s ease-in-out 2.5s forwards',
       },
+
+      keyframes:{
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+        fadeOut: {    
+          '0%': { opacity: '1' },
+          '100%': { opacity: '0' },
+        },
+      },
+
     },
   },
   plugins: [],
