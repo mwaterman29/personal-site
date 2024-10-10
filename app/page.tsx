d5986d77189e629cd08fadc2aac6608ae47907f5
Matt <mwaterman29@gmail.com>
2024-10-09 20:37:48 -0400

    fix home images

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,17 @@ export default function Home() {
       <div className='flex basis-4/12 mx-4 rounded-md h-1/2 md:h-full'>
         <Carousel
           urls={[
-            'https://i.gyazo.com/0f24aeb18f1ecd52f50f47c6c527dd91.jpg', //'https://cdn.discordapp.com/attachments/530910055072137266/1178927786690363443/IMG_0738.jpg?ex=6577ed0f&is=6565780f&hm=234a5ffb57fcd512869c938ce2f3b13c755f7759c6da53f5360d34d3c3122d90&',
+            /*'https://i.gyazo.com/0f24aeb18f1ecd52f50f47c6c527dd91.jpg', //'https://cdn.discordapp.com/attachments/530910055072137266/1178927786690363443/IMG_0738.jpg?ex=6577ed0f&is=6565780f&hm=234a5ffb57fcd512869c938ce2f3b13c755f7759c6da53f5360d34d3c3122d90&',
             'https://i.gyazo.com/db62bfc42b2721441915f0924d098fc6.jpg', //'https://cdn.discordapp.com/attachments/530910055072137266/1178927785088139364/IMG_2897.jpg?ex=6577ed0f&is=6565780f&hm=3ae500720f1d055320900d20fd0029ea3736c1d128d6d20a91c0754d2b1753aa&',
             'https://cdn.discordapp.com/attachments/530910055072137266/1178927785683718234/IMG_7660.jpg?ex=6577ed0f&is=6565780f&hm=de0657bee04c5b6f051d0abfb7d9fe7afc66dc8bd4b1c3968ca25c4529e58840&',
             'https://cdn.discordapp.com/attachments/530910055072137266/1178927786124124290/00A59270-3682-4F70-9139-3FC263C22CE0.jpg?ex=6577ed0f&is=6565780f&hm=bdfd6b90b636d15dbc7089c1f05fac5dfcca883e4b0d9d60686d804f8577303e&',
             'https://i.gyazo.com/968dd60a5edc94ad4306a696b5496a86.jpg', //'https://cdn.discordapp.com/attachments/530910055072137266/1178927878193295360/IMG_8882.jpg?ex=6577ed25&is=65657825&hm=d2c824b56001e288b99a773eba5c4653173bea6cbf5fe56a308e243beeadf416&',
+            */
+            '/me_1.jpg',
+            '/me_2.jpg',
+            '/me_3.jpg',
+            '/me_4.jpg',
+            '/me_5.jpg',
           ]}
           />
       </div>
