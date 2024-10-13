de8864fd9db1d00730f2af545e745a58b4c39b1d
Matt <mwaterman29@gmail.com>
2024-10-13 19:32:51 -0400

    stop logs

--- app/music/[name]/page.tsx
+++ app/music/[name]/page.tsx
@@ -55,7 +55,7 @@ export default async function PostPage({ params }: { params: { name: string } })
     
     let content = await markdownToComponent(contentSlice, []); // extraRules);
 
-    console.log(contentSlice);
+    //console.log(contentSlice);
 
     if(album)
     {
