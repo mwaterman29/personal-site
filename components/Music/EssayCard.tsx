5790b1664c4d72e7f23f27ba0c7d1a6d70ba6d49
Matt <mwaterman29@gmail.com>
2025-06-18 20:06:11 -0400

    essay

--- components/Music/EssayCard.tsx
+++ components/Music/EssayCard.tsx
@@ -9,6 +9,8 @@ const EssayCard = ({ essay }: { essay: Essay }) =>
 	return (
 		<Link key={essay.id} href={`/music/essays/${essay.fileLink}`} className='flex flex-col border rounded-md gap-y-2 items-start w-full justify-between group'>
 			<div className='flex flex-col gap-y-2 w-full h-full justify-between'>
+
+
 				<div className='flex flex-row gap-2 px-6 pt-4 items-start min-h-[px]'>
 					<div className='flex flex-col'>
 						<p className='text-xl font-bold line-clamp-2' title={essay.title}>
@@ -17,12 +19,13 @@ const EssayCard = ({ essay }: { essay: Essay }) =>
 					</div>
 				</div>
 
+				<div className='flex flex-col gap-y-1'>
 				<div className='flex flex-row items-center justify-between text-sm px-6 pb-2'>
 					<p>Published on {format(essay.createdAt, 'MMMM dd, yyyy')}</p>
 					<p className='p-1 bg-purple-400 rounded-md'>Essay</p>
 				</div>
 
-				<div className='relative overflow-hidden w-full'>
+				<div className='relative overflow-hidden w-full aspect-square'>
 					<img className={cn('w-full h-auto object-cover transition-all duration-300 group-hover:brightness-50')} alt='Preview Image' src={essay.imageLink} />
 					{essay.description && (
 						<div className='absolute inset-0 flex items-center justify-center p-4 opacity-0 group-hover:opacity-100 transition-opacity duration-300'>
@@ -30,6 +33,9 @@ const EssayCard = ({ essay }: { essay: Essay }) =>
 						</div>
 					)}
 				</div>
+				</div>
+
+
 			</div>
 		</Link>
 	);
