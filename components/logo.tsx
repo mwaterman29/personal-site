5c5aead895842e710de4624dadc34e1bd7c44129
Matt <mwaterman29@gmail.com>
2025-07-01 23:13:13 -0400

    painstaking manually adjusted svg for logo

---
+++ components/logo.tsx
@@ -0,0 +1,34 @@
+import { cn } from "@/lib/utils"
+
+const Logo = ({ className }: { className?: string }) =>
+{
+	return (
+		<svg
+			className={cn('w-full max-w-4xl h-48 svg-exclude-fill', className)}
+			viewBox='0 0 400 400'
+			xmlns='http://www.w3.org/2000/svg'
+			stroke='white'
+			fill='none'
+			stroke-width='10'
+		>
+			<circle cx='200' cy='200' r='180' />
+			<path
+				stroke-width='10'
+				d='
+M20,200 
+L40,200
+C75,200,75,300,100,300
+C125,320,125,80,150,80
+C175,80,175,320,200,320
+C225,320,225,80,250,80 
+C275,80,275,300,300,300
+C325,300,325,200,350,200
+L380,200
+'
+			/>
+			{/* <line x1='200' y1='20' x2='200' y2='380' stroke-width='1' stroke='white' /> */}
+		</svg>
+	);
+};
+
+export default Logo;
